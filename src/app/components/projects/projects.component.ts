c9c9565a1e6515992b104d0e909b56423be37fb7
Alan Sato <Alan.Sato@toyota.com>
2019-02-15 16:10:52 -0600

    add some on load functionality

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { Projects } from 'src/app/models/Projects';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-projects',
@@ -9,13 +11,28 @@ import { Projects } from 'src/app/models/Projects';
 })
 export class ProjectsComponent implements OnInit {
 
+  public newProjectForm: boolean;
   public projects = Array<Projects>();
-  constructor(private apiService: ApiService) { }
+  public loggedIn: Observable<boolean>;
+  constructor(private apiService: ApiService, private authService: AuthService) { }
 
   ngOnInit() {
     this.apiService.getUserProjects(1).subscribe((data: Array<Projects>) => {
       this.projects = data;
+      this.onProjectsLoaded();
+    }, error =>{
+      // Need better error handling -- display page load error or redirect back to home
+      console.log(error);
     });
+  }
+
+  public onProjectsLoaded(){
+    this.newProjectForm = false;
+    this.loggedIn = this.authService.isLoggedIn;
+  }
+
+  public newProjectButton(){
+    this.newProjectForm = true;
   }
 
 
